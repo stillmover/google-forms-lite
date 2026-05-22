fcaa234c20bf4280429d34b21b88dc6fbdd23283
Viktoriia <struk.victoria@gmail.com>
2026-05-22 17:16:59 +0300

    refactor: remove the dispatch entirely

--- client/src/hooks/useHomePage.ts
+++ client/src/hooks/useHomePage.ts
@@ -1,20 +1,9 @@
-import { useEffect } from 'react';
 import { useFormsQuery } from '../api/enhancedApi';
-import { useAppDispatch } from '../store/hooks';
-import { setForms } from '../store/formsSlice';
 
 export function useHomePage() {
-  const dispatch = useAppDispatch();
-
   const query = useFormsQuery(undefined, {
     refetchOnMountOrArgChange: true,
   });
-
-  useEffect(() => {
-    if (query.data?.forms) {
-      dispatch(setForms(query.data.forms));
-    }
-  }, [query.data, dispatch]);
 
   return {
     forms: query.data?.forms ?? [],
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { api } from '../api/enhancedApi'
 import { formsReducer } from './formsSlice'
-import { responsesReducer } from './responsesSlice'
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     forms: formsReducer,
-    responses: responsesReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
