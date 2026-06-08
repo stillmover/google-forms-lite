6f1b9546fadcb48cd59e6ef08f19f832ad64f115
Viktoriia <struk.victoria@gmail.com>
2026-06-08 22:10:09 +0300

    chore: add serve package

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,12 +7,4 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: true,
-  },
-  preview: {
-    host: '0.0.0.0',
-    allowedHosts: true,
-  },
 });
